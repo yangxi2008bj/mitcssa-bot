b30a10a1719c4a4314f3bdfb65bf985901c8a694
yangxi2008bj <yangxi2008bj@gmail.com>
2022-10-22 13:48:47 -0700

    create two models based on the new tables

--- entity/models.py
+++ entity/models.py
@@ -57,3 +57,59 @@ class Message(Base):
         self.touser = touser
         self.type = type
         self.content = content
+
+
+class Decoder(Base):
+    __tablename__ = 'im_wx_user_decoder'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    wId = Column(String(100))
+    goal_type = Column(Integer)
+    gender = Column(Integer)
+    birth_year_min = Column(Integer)
+    birth_year_max = Column(Integer)
+    school = Column(String(20))
+    accp_program = Column(String(20))
+    hobby = Column(Integer)
+    hobby_match = Column(Integer)
+    created_at = Column(DateTime, server_default=func.now(), onupdate=func.now(), comment='创建时间')
+    updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now(), comment='创建时间')
+
+    def __init__(self, id, wId, goal_type, gender, birth_year_min, birth_year_max, school, accp_program, hobby, hobby_match):
+        self.id = id
+        self.wId = wId
+        self.goal_type = goal_type
+        self.gender = gender
+        self.birth_year_min = birth_year_min
+        self.birth_year_max = birth_year_max
+        self.school = school
+        self.accp_program = accp_program
+        self.hobby = hobby
+        self.hobby_match = hobby_match
+
+
+class Encoder(Base):
+    __tablename__ = 'im_wx_user_encoder'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    wId = Column(String(100))
+    goal_type = Column(Integer)
+    gender = Column(Integer)
+    birth_year = Column(Integer)
+    school = Column(String(20))
+    program = Column(String(20))
+    hobby = Column(Integer)
+    max_decoding = Column(Integer)
+    contact_pref = Column(String(10))
+    created_at = Column(DateTime, server_default=func.now(), onupdate=func.now(), comment='创建时间')
+    updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now(), comment='创建时间')
+
+    def __init__(self, id, wId, goal_type, gender, birth_year, school, program, hobby, max_decoding, contact_pref):
+        self.id = id
+        self.wId = wId
+        self.goal_type = goal_type
+        self.gender = gender
+        self.birth_year = birth_year
+        self.school = school
+        self.program = program
+        self.hobby = hobby
+        self.max_decoding = max_decoding
+        self.contact_pref = contact_pref
